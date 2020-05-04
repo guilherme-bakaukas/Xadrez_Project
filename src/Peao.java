db4edeb32455926d4ed0064c0708656ab341785d
guilherme-bakaukas <61765419+guilherme-bakaukas@users.noreply.github.com>
2020-05-04 19:57:54 -0300

    verificação de movimento para peao, torre e rei

--- src/Peao.java
+++ src/Peao.java
@@ -5,4 +5,26 @@ public class Peao extends Peca {
         if (equipe=='P') this.nome='P';
         else if (equipe=='B') this.nome='p';
     }
+
+    public boolean verifica_movimento(Peca[][] matriz, int[] vetor_pos){
+
+        // talvez tenha que colocar o caso da primeira rodada, em que o peao pode andar duas casas
+
+        int linha_inicial=vetor_pos[0];
+        int coluna_inicial=vetor_pos[1];
+        int linha_final=vetor_pos[2];
+        int coluna_final=vetor_pos[3];
+
+        if (super.verifica_movimento(matriz,vetor_pos)==false) return false;
+
+        if (coluna_final!=coluna_inicial) return false;
+        switch (this.equipe){
+            case('P'):
+                if (linha_final-linha_inicial!=1) return false;
+            case('B'):
+                if (linha_final-linha_inicial!=-1) return false;
+        }
+
+        return true;
+    }
 }
--- src/Peca.java
+++ src/Peca.java
@@ -5,4 +5,13 @@ public class Peca {
         if (equipe=='P') this.equipe='P';
         if (equipe=='B') this.equipe='B';
     }
+
+    public boolean verifica_movimento(Peca[][] matriz, int[] vetor_pos){
+        int linha_inicial=vetor_pos[0];
+        int coluna_inicial=vetor_pos[1];
+        int linha_final=vetor_pos[2];
+        int coluna_final=vetor_pos[3];
+        if (matriz[linha_final][coluna_final]!=null) return false;
+        return true;
+    }
 }
