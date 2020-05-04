f46b532a989567cde4ba78233d0e9b0bf1dc8516
guilherme-bakaukas <61765419+guilherme-bakaukas@users.noreply.github.com>
2020-05-04 13:20:51 -0300

    ajuste da CSVReader, criação da herdeira Objects e criação de um Data.csv teste, ajuste na main para coletar os dados do arquivo CSV

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import CSVReader_extension.*;
 public class Main {
     public static void main(String[] args){
         Tabuleiro tab=new Tabuleiro();
         tab.print_tabuleiro();
+        Objects csv = new Objects();
+        csv.setDataSource("C:/Users/guilh/IdeaProjects/Xadrez_Project/src/CSVReader_extension/Data.csv");
+        Objects commands[] = csv.transforma_Objects();
     }
 
 }
